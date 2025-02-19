5d203b5388f956e1905a3f3140684c794760b863
puppysilverstar <james.harrison981208@gmail.com>
2025-02-19 16:23:30 +0900

    progress before the static files by puppysilverstar

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
+    path('admin/', admin.site.urls),
     path('members/', views.members, name="members"),
+    path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
 ]
